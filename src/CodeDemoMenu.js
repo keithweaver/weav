749b45d8eae4461be9a950761805cfb99677b251
Keith Weaver <keithmweaver@gmail.com>
2018-03-07 10:16:00 -0500

    Added more

--- src/CodeDemoMenu.js
+++ src/CodeDemoMenu.js
@@ -3,14 +3,19 @@ import React from 'react';
 import CodeDemoMenuItem from './CodeDemoMenuItem';
 
 const CodeDemoMenu = (props) => {
+  const {
+    uiComponents,
+    onUIComponentChange,
+  } = props;
   const itemWidth = 100 / uiComponents;
+
   return (
     <div>
       {
-        (props.uiComponents).map(uiComponent => (
+        (uiComponents).map(uiComponent => (
           <CodeDemoMenuItem
             uiComponent={uiComponent}
-            onUIComponentChange={props.onUIComponentChange}
+            onUIComponentChange={onUIComponentChange}
             width={itemWidth}
           />
         ))
